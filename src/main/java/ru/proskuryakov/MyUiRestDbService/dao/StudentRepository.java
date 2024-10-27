ede9cea0f784fd8b820d50aaea27a32638d5418d
Freaverx <deimos.sergei@yandex.ru>
2024-10-27 22:09:47 +0500

    Initial commit

---
+++ src/main/java/ru/proskuryakov/MyUiRestDbService/dao/StudentRepository.java
@@ -0,0 +1,10 @@
+package ru.proskuryakov.MyUiRestDbService.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.proskuryakov.MyUiRestDbService.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
